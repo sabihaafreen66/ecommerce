98e2b116238389366e7c48d0a1e4ec124bfcbc3b
SabihaAfreen <afrah9059@gmail.com>
2024-05-19 11:39:40 +0530

    task5commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { CartProvider } from './Product/CartContext';
+import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Home = () => {
   return (
     <div>
-      <iframe title="Home Page" src="https://prasadyash2411.github.io/ecom-website/index.html#" width="100%" height="800px" frameBorder="0"></iframe>
+      <iframe
+        title="Home Page"
+        src="https://prasadyash2411.github.io/ecom-website/index.html#"
+        width="100%"
+        height="800px"
+        frameBorder="0"
+      ></iframe>
     </div>
   );
 };
@@ -14,7 +19,13 @@ const Home = () => {
 const Store = () => {
   return (
     <div>
-      <iframe title="Store Page" src="https://prasadyash2411.github.io/ecom-website/dynamicstore.html" width="100%" height="800px" frameBorder="0"></iframe>
+      <iframe
+        title="Store Page"
+        src="https://prasadyash2411.github.io/ecom-website/dynamicstore.html"
+        width="100%"
+        height="800px"
+        frameBorder="0"
+      ></iframe>
     </div>
   );
 };
@@ -22,27 +33,39 @@ const Store = () => {
 const About = () => {
   return (
     <div>
-      <iframe title="About Page" src="https://prasadyash2411.github.io/ecom-website/about.html" width="100%" height="800px" frameBorder="0"></iframe>
+      <iframe
+        title="About Page"
+        src="https://prasadyash2411.github.io/ecom-website/about.html"
+        width="100%"
+        height="800px"
+        frameBorder="0"
+      ></iframe>
+      <div className="d-flex justify-content-around mt-4">
+        <NavLink to="https://www.youtube.com" target="www.youtube.com" className="btn btn-danger">
+          YouTube
+        </NavLink>
+        <NavLink to="https://www.spotify.com" target="_blank" className="btn btn-success">
+          Spotify
+        </NavLink>
+        <NavLink to="https://www.facebook.com" target="_blank" className="btn btn-primary">
+          Facebook
+        </NavLink>
+      </div>
     </div>
   );
 };
 
 const App = () => {
   return (
-    <CartProvider>
-      <Router>
-        <div className="container">
-          <header className="my-4">
-            <h1>My E-commerce Store</h1>
-          </header>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/store" element={<Store />} />
-          </Routes>
-        </div>
-      </Router>
-    </CartProvider>
+    <Router>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/store" element={<Store />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
